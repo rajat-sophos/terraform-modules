29917d27cfd5d10435fb63489f9a8b2ca22f3fcb
rajat-sophos <rajat@avidsecure.io>
2022-09-22 16:04:06 +0530

    test commit

--- modules/aws/acm_certificate/main.tf
+++ modules/aws/acm_certificate/main.tf
@@ -8,8 +8,6 @@ resource "aws_acm_certificate" "cert" {
   subject_alternative_names = var.subject_alternative_names
   tags                      = var.tags
 
-
-
   lifecycle {
     create_before_destroy = true
   }
